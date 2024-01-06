1cf01bb0784871a0f42a079ed9a39d69256a6bc7
d-hurricane <dhurricane@yandex.ru>
2024-01-06 14:01:44 +0400

    1.14 Создание контроллеров
    Реализовано отображение главной страницы и страницы с товарами.

--- store-server/store/store/urls.py
+++ store-server/store/store/urls.py
@@ -16,6 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+import products.views as products
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', products.index, name='index'),
+    path('products/', products.products, name='products'),
 ]
