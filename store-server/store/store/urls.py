43511cad265a5965323e3a399b13d8af54866e69
d-hurricane <dhurricane@yandex.ru>
2024-01-08 00:52:52 +0400

    4.2 Пространство имён в диспетчере URL
    Разделено описание url-ссылок корневых и принадлежащих приложению products.

--- store-server/store/store/urls.py
+++ store-server/store/store/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
@@ -24,7 +24,7 @@ import products.views as products
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', products.index, name='index'),
-    path('products/', products.products, name='products'),
+    path('products/', include('products.urls', namespace='products')),
 ]
 
 if settings.DEBUG:
