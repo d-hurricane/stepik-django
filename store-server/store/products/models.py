635715395660fe42ad7436bbb42d7890ed63e3ce
d-hurricane <dhurricane@yandex.ru>
2024-01-07 18:40:33 +0400

    3.6 Работа с моделями через админ панель
    Для товаров и категорий добавлены представления объектов, а также их таблицы данных выведены в админ-панель.

--- store-server/store/products/models.py
+++ store-server/store/products/models.py
@@ -1,9 +1,21 @@
 from django.db import models
 
 
+def catalog_presentation(instance):
+    result = str(instance.name)
+    if result.isspace():
+        result = '<>'
+    code = str(instance.id) if instance.id else 'create'
+    result = '{} ({})'.format(result, code)
+    return result
+
+
 class ProductCategory(models.Model):
     name = models.CharField(max_length=128)
     description = models.TextField(blank=True)
+
+    def __str__(self):
+        return catalog_presentation(self)
 
 
 class Product(models.Model):
@@ -13,3 +25,6 @@ class Product(models.Model):
     image = models.ImageField(upload_to='product_images')
     price = models.DecimalField(max_digits=15, decimal_places=2)
     quantity = models.PositiveIntegerField()
+
+    def __str__(self):
+        return catalog_presentation(self)
