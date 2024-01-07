face110d452d3a283bd804cd4fe2424d40dd4a7e
d-hurricane <dhurricane@yandex.ru>
2024-01-07 12:24:42 +0400

    3.3 Модели в Django. Создание первых моделей

--- store-server/store/products/models.py
+++ store-server/store/products/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class ProductCategory(models.Model):
+    name = models.CharField(max_length=128)
+    description = models.TextField(blank=True)
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=128)
+    description = models.TextField(blank=True)
+    category = models.ForeignKey(ProductCategory)
+    image = models.ImageField(upload_to='product_images')
+    price = models.DecimalField(max_digits=15, decimal_places=2)
+    quantity = models.PositiveIntegerField()
